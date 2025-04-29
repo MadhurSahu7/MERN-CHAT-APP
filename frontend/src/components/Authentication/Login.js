84b2da1dea1d7502ce40ca421861a660a21f1ba0
Madhur Sahu <sahumadhur6@gmail.com>
2025-04-29 20:06:07 +0530

    first

---
+++ frontend/src/components/Authentication/Login.js
@@ -0,0 +1,113 @@
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { useToast } from '@chakra-ui/react'
+import axios from 'axios'
+import {useHistory} from 'react-router-dom'
+import { ChatState } from '../../Context/ChatProvider'
+
+const Login = () => {
+  const [show, setShow] = useState(false)
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const toast = useToast();
+  const history = useHistory();
+  const { setUser } = ChatState();
+  
+  const handleClick = () =>{
+    setShow(!show)
+  }
+  
+  const submitHandler = async () =>{
+    setLoading(true);
+    if(!email || !password){
+      toast({
+        title: 'Please fill all the fields',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position:'bottom'
+      })
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers:{
+          "Content-type":"application/json"
+        }
+      };
+
+      const {data} = await axios.post(
+        "/api/user/login",
+        {email,password},
+        config
+      );
+
+      toast({
+        title: 'Login successful',
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+        position:'bottom'
+      });
+      setUser(data);
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: 'Error Occured!',
+        description: error.response.data.message,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position:'bottom'
+      })
+      setLoading(false);
+    }
+  }
+  
+    return (
+      <VStack spacing='5px' color='black'>
+
+  <FormControl id='email' isRequired>
+      <FormLabel>Email</FormLabel>
+      <Input placeholder='Enter your email' onChange={(event)=> setEmail(event.target.value)}
+      value={email}
+      />
+  </FormControl>
+  <FormControl id='password' isRequired>
+      <FormLabel>Password</FormLabel>
+      <InputGroup size='md'>
+      <Input type={show ? "text" : "password"} placeholder='Password' value={password} onChange={(event)=> setPassword(event.target.value)}/>
+      <InputRightElement width='4.5rem'>
+      <Button h='1.75rem' size='sm' onClick={handleClick}>
+        {show ? 'Hide' :'Show'}
+      </Button>
+      </InputRightElement>
+      </InputGroup>
+     
+  </FormControl>
+
+  <Button colorScheme='blue' w='100%' style={{marginTop:15}}
+  isLoading={loading}
+  onClick={submitHandler}
+  >
+  Login
+  </Button>
+  <Button variant='solid' colorScheme='red' w='100%'
+  onClick={()=>{
+    setEmail('guest@example.com');
+    setPassword('123456');
+  }}
+  >
+  Get Guest user credentials
+  </Button>
+      </VStack>
+    )
+}
+
+export default Login
